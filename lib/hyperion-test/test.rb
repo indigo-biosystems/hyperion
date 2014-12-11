530df295310688ddbb944b503dd5c04ab2777bf8
Dallas Treder <dtreder@indigobio.com>
2014-12-11 14:13:24 -0500

    Fixed header comparison for Hyperion::fake

--- lib/hyperion-test/test.rb
+++ lib/hyperion-test/test.rb
@@ -23,15 +23,16 @@ class Hyperion
     end
 
     def run_fake_server(setup)
-      routes = setup.rules.map{|r| [r.method, r.path]}.uniq
+      routes = setup.rules.map { |r| [r.method, r.path] }.uniq
       this = self
       Mimic.mimic(port: @fake_port) do
         routes.each do |(method, path)|
           send(method, path) do
-            matched = setup.rules.
-                select{|r| r.method == method && r.path == path}.
-                detect{|r| this.send(:is_subhash, self.headers, headers)}
-            matched.handler.call(this.send(:make_req_obj, request.body.read, self.request.env['CONTENT_TYPE']))
+            path_matched = setup.rules.select { |r| r.method == method && r.path == path }
+            matched = path_matched.detect { |r|
+              this.send(:sinatrize_headers, r.headers).subhash?(request.env)
+            }
+            matched.handler.call(this.send(:make_req_obj, request.body.read, request.env['CONTENT_TYPE']))
           end
         end
       end
@@ -44,8 +45,22 @@ class Hyperion
 
     private
 
+    def sinatrize_headers(headers)
+      Hash[headers.map { |k, v| [sinatra_header(k), v] }]
+    end
+
+    def sinatra_header(header)
+      cased_header = header.upcase.gsub('-', '_')
+      case cased_header
+        when 'ACCEPT'; 'HTTP_ACCEPT'
+        when 'HOST'; 'HTTP_HOST'
+        when 'USER_AGENT'; 'HTTP_USER_AGENT'
+        else; cased_header
+      end
+    end
+
     def remapped_base_uri(base_uri)
-      base_uri_mapping.fetch(base_uri) {|x|x}
+      base_uri_mapping.fetch(base_uri) { |x| x }
     end
 
     def base_uri_mapping
@@ -58,7 +73,7 @@ class Hyperion
     end
 
     def is_subhash(hash, subhash)
-      subhash.each_pair.all?{|k, v| hash[k] == v}
+      subhash.each_pair.all? { |k, v| hash[k] == v }
     end
 
     class Setup
@@ -79,5 +94,11 @@ class Hyperion
 
   def uri_base
     self.class.send(:remapped_base_uri, @uri_base)
+  end
+end
+
+class Hash
+  def subhash?(hash)
+    each_pair.all? { |k, v| hash[k] == v }
   end
 end
--- spec/lib/hyperion/test_spec.rb
+++ spec/lib/hyperion/test_spec.rb
@@ -13,18 +13,18 @@ describe Hyperion do
   # end
 
   shared_examples 'a web server' do
+    let(:user_response_params) { Hyperion::ResponseParams.new('user', 1, :json) }
     it 'implements specific routes' do
-      Hyperion.send(type, 'http://yoursite.com:3000') do |svr|
+      create_fake_server do |svr|
         svr.allow(:get, '/users/0') do
-          [200, {'Content-Type' => 'application/json'}, write({'name' => 'freddy'}, :json)]
+          success_response({'name' => 'freddy'})
         end
         svr.allow(:post, '/say_hello') do |req|
-          [200, {'Content-Type' => 'application/json'}, write({'greeting' => "hello, #{req.body['name']}"}, :json)]
+          success_response({'greeting' => "hello, #{req.body['name']}"})
         end
       end
 
-      response_params = Hyperion::ResponseParams.new('user', 1, :json)
-      result = Hyperion.get('http://yoursite.com:3000/users/0', response_params)
+      result = Hyperion.get('http://yoursite.com:3000/users/0', user_response_params)
       expect(result.code).to eql 200
       expect(result.status).to eql Hyperion::Result::Status::SUCCESS
       expect(result.body).to eql({'name' => 'freddy'})
@@ -36,9 +36,69 @@ describe Hyperion do
       expect(result.body).to eql({'greeting' => 'hello, freddy'})
     end
 
-    it 'considers the HTTP method to be part of the route'
-    it 'considers the path to be part of the route'
-    it 'considers the headers to be part of the route'
+    it 'considers the HTTP method to be part of the route' do
+      create_fake_server do |svr|
+        svr.allow(:get, '/users/0') do
+          success_response({'name' => 'freddy'})
+        end
+        svr.allow(:post, '/users/0') do |req|
+          success_response({'updated' => {'name' => req.body['name']}})
+        end
+      end
+      result = Hyperion.get('http://yoursite.com:3000/users/0', user_response_params)
+      expect(result.body).to eql({'name' => 'freddy'})
+
+      result = Hyperion.post('http://yoursite.com:3000/users/0', user_response_params, write({'name' => 'annie'}, :json), :json)
+      expect(result.body).to eql({'updated' => {'name' => 'annie'}})
+    end
+
+    it 'considers the path to be part of the route' do
+      create_fake_server do |svr|
+        svr.allow(:get, '/users/0') do
+          success_response({'name' => 'freddy'})
+        end
+        svr.allow(:get, '/users/1') do
+          success_response({'name' => 'annie'})
+        end
+      end
+
+      result = Hyperion.get('http://yoursite.com:3000/users/0', user_response_params)
+      expect(result.body).to eql({'name' => 'freddy'})
+
+      result = Hyperion.get('http://yoursite.com:3000/users/1', user_response_params)
+      expect(result.body).to eql({'name' => 'annie'})
+    end
+
+    it 'considers the headers to be part of the route' do
+      create_fake_server do |svr|
+        svr.allow(:get, '/users/0', {'Accept' => 'application/vnd.indigobio-ascent.user-v1+json'}) do
+          success_response({'name' => 'freddy'})
+        end
+        svr.allow(:get, '/users/0', {'Accept' => 'application/vnd.indigobio-ascent.full_user-v1+json'}) do
+          success_response({'first_name' => 'freddy', 'last_name' => 'kruger', 'address' => 'Elm Street'})
+        end
+      end
+      result = Hyperion.get('http://yoursite.com:3000/users/0', user_response_params)
+      expect(result.body).to eql({'name' => 'freddy'})
+
+      full_user_response_params = Hyperion::ResponseParams.new('full_user', 1, :json)
+      result = Hyperion.get('http://yoursite.com:3000/users/0', full_user_response_params)
+      expect(result.body).to eql({'first_name' => 'freddy', 'last_name' => 'kruger', 'address' => 'Elm Street'})
+    end
+
+    it 'considers the domain to be part of the route'
+    # TODO: i.e., 'hello.com' vs 'goodbye.com'; since we are wiping out the domain as part of using Mimic,
+    # TODO: we need to find a way to add the domain (and maybe protocol as below) to mimic.
+    it 'considers the protocol to be part of the route' #TODO: should this actually be true?
+
+    def create_fake_server(opts={}, &routes)
+      base_uri = "#{opts[:proto] || 'http'}://#{opts[:domain] || 'yoursite.com'}:#{opts[:port] || 3000}"
+      Hyperion.send(type, base_uri, &routes)
+    end
+
+    def success_response(body)
+      [200, {'Content-Type' => 'application/json'}, write(body, :json)]
+    end
   end
 
   # describe '::stub' do
