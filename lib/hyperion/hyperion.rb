0595aedd38fff85785a44071a7c00c88f133a75c
Justin Grimes <grimesj@indigobio.com>
2015-11-11 14:26:03 -0500

    Update hyperion.rb

--- lib/hyperion/hyperion.rb
+++ lib/hyperion/hyperion.rb
@@ -53,10 +53,7 @@ class Hyperion
   end
 
   def build_headers(additional_headers)
-    puts "AdditionalHeaders: " + additional_headers.inspect
-    new_h = route_headers(route).merge(additional_headers)
-    puts "Merged Headers: " new_h.inspect
-    new_h
+    route_headers(route).merge(additional_headers)
   end
 
   def hyperion_result_for(typho_result, dispatch)
